c775de4509e5f03cb6eda04186d515942af1c219
Petro <petro@petro.ws>
2025-11-30 22:37:28 +0100

    Add docker-builder script stub

--- bin/build_docker.py
+++ bin/build_docker.py
@@ -2,14 +2,36 @@
 """
 Build Docker images.
 """
+import argparse
 
 
 def main():
     """
     Entry point function.
     """
-    print("Hello, World!")
-    print("Building Docker images...")
+
+    argparser = argparse.ArgumentParser(description="Build Docker images.")
+    argparser.add_argument(
+        "--config",
+        type=str,
+        default="docker.yml",
+        help="Path to the Docker build configuration file.",
+    )
+    argparser.add_argument(
+        "--detect-tags",
+        action="store_true",
+        help="Detect new tags for Docker images and print list.",
+    )
+    args = argparser.parse_args()
+    config_path = args.config
+    print(f"Using configuration file: {config_path}")
+
+
+def read_config():
+    """
+    Reads configuration for building Docker images.
+    """
+    pass
 
 
 if __name__ == "__main__":
